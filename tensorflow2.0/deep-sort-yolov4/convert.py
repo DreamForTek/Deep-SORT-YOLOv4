a633529b8985c3406d2997242c271f10bd3e3a3f
Rui Sebastiao <ruisebastiao@gmail.com>
2020-09-18 14:11:06 +0100

    updted

--- tensorflow2.0/deep-sort-yolov4/convert.py
+++ tensorflow2.0/deep-sort-yolov4/convert.py
@@ -7,6 +7,19 @@ from keras.models import load_model
 from keras.layers import Input
 import tensorflow as tf
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+  try:
+    tf.config.experimental.set_virtual_device_configuration(
+        gpus[0],
+        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)])
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Virtual devices must be set before GPUs have been initialized
+    print(e)
+
 from yolo4.model import yolo_eval, yolo4_body
 from yolo4.utils import letterbox_image
 
@@ -174,7 +187,7 @@ if __name__ == '__main__':
 
     score = 0.5
     iou = 0.5
-
+    
     yolo4_model = Yolo4(score, iou, anchors_path, classes_path, model_path, weights_path)
 
     yolo4_model.close_session()
